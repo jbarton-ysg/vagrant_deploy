e377176632caed7e3076dcb5275dcadd53b44993
Jeremy Barton <jbarton@ysgco.com>
2019-08-16 11:06:58 -0600

    update script and Vagrantfile

--- myyesgo_vagrant.sh
+++ myyesgo_vagrant.sh
@@ -9,9 +9,13 @@ pwd
 # run maven build
 mvn -B verify
 
-cp myyesgo-api/target/myyesgo-api-1.0.0-SNAPSHOT.war /opt/tomcat/webapps/myyesgo-api.war
-cp myyesgo-security/target/security-rest-api-1.0.0-SNAPSHOT.war /opt/tomcat/webapps//security-rest-api.war
-cp myyesgo-integration/target/myyesgo-integration-1.0.0-SNAPSHOT.war /opt/tomcat/webapps/myyesgo-integration.war
+mv myyesgo-api/target/myyesgo-api-1.0.0-SNAPSHOT.war myyesgo-api.war
+mv myyesgo-security/target/security-rest-api-1.0.0-SNAPSHOT.war security-rest-api.war
+mv myyesgo-integration/target/myyesgo-integration-1.0.0-SNAPSHOT.war myyesgo-integration.war
+
+sudo cp myyesgo-api/target/myyesgo-api.war /opt/tomcat/webapps/
+sudo cp myyesgo-security/target/security-rest-api.war /opt/tomcat/webapps/
+sudo cp myyesgo-integration/target/myyesgo-integration.war /opt/tomcat/webapps/
 
 cd myyesgo-web
 rm -rf node_modules
@@ -19,9 +23,7 @@ rm -rf package-lock.json
 npm install
 npm run build:vagrant
 mv build ROOT
-cp -r ROOT /webapps
-
-
+cp -r ROOT webapps/
 
 sh /opt/tomcat/bin/startup.sh
 
@@ -33,3 +35,14 @@ rm -rf /opt/tomcat/webapps/*.war
 rm -rf /opt/tomcat/logs/*
 
 sh /opt/tomcat/bin/startup.sh
+
+#update database
+cd ../myyesgo-database
+mysql --user="remote" --password="password" --execute="DROP DATABASE myyesgo; CREATE DATABASE myyesgo;"
+mysql --user="remote" --password="password" --execute="DROP DATABASE myyesgo_security; CREATE DATABASE myyesgo_security;"
+mysql --user="remote" --password="password" --execute="DROP DATABASE myyesgo_integration; CREATE DATABASE myyesgo_integration;"
+
+mysql -u remote -p"secret" myyesgo < Scripts/myyesgo.sql
+mysql -u remote -p"secret" myyesgo_security < Scripts/myyesgo_security.sql
+mysql -u remote -p"secret" myyesgo_integration < Scripts/myyesgo_integration.sql
+mysql -u remote -p"secret" myyesgo < Procedures/spGetHandOff.sql
